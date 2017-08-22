9660ec9ffa05c56070a6da8a89ad691f7cf405b8
Aleksander Chromik <achromik@gmail.com>
2017-08-23 01:04:27 +0200

    Excercise 8.2 finished

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var name = prompt('Hi, my name is Alex! \nWhat is your name?');
+alert('Hello, ' + name + '! Welcome to my page! Nice to see you!');
+console.log('Hello, ' + name + '! Welcome to my page! Nice to see you!');
